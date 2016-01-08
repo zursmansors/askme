cdfc9bda7c15f852a220489e4dc7afef73425064
zursmansors <zursmansors@gmail.com>
2016-01-08 18:30:19 +0200

    Lesson_8. Polymorphic attachments. Answer associated with attachments

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it { should belong_to :user }
   it { should belong_to :question }
+  it { should have_many :attachments }
 
   it { should validate_presence_of :user_id }
   it { should validate_presence_of :question_id }
   it { should validate_presence_of :body }
+
+  it { should accept_nested_attributes_for :attachments }
 
 
   let(:user) { create(:user) }
